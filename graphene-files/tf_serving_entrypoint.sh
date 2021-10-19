9baf8cabb34bf80b04438124ea51f0bddd2686c9
Daniel Weiße <66256922+daniel-weisse@users.noreply.github.com>
2021-10-19 15:25:32 +0200

    Update to use Gramine release v1.0 (#11)

--- graphene-files/tf_serving_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-unset http_proxy && unset https_proxy
-
-LD_LIBRARY_PATH="/opt/intel/sgx-aesm-service/aesm/:$LD_LIBRARY_PATH" /opt/intel/sgx-aesm-service/aesm/aesm_service
-
-graphene-sgx tensorflow_model_server
